6f308ae0473636d7974cd58ca518fc08417cf517
ortlieb3 <gustavooortlieb@gmail.com>
2025-06-11 13:53:52 -0300

    programei um clique no button 2 do frmPrincipal para abrir o frmCadProdutos

--- provasextagu/Form1.Designer.cs
+++ provasextagu/Form1.Designer.cs
@@ -34,7 +34,7 @@
             // comboBox1
             // 
             comboBox1.FormattingEnabled = true;
-            comboBox1.Location = new Point(445, 311);
+            comboBox1.Location = new Point(404, 242);
             comboBox1.Name = "comboBox1";
             comboBox1.Size = new Size(151, 28);
             comboBox1.TabIndex = 0;
