0b1f6e9c8f26750c49fa00aa4c5a72c570585393
ortlieb3 <gustavooortlieb@gmail.com>
2025-06-11 03:02:50 -0300

    terminei frmLoguin

--- provasextagu/Form1.Designer.cs
+++ provasextagu/Form1.Designer.cs
@@ -34,7 +34,7 @@
             // comboBox1
             // 
             comboBox1.FormattingEnabled = true;
-            comboBox1.Location = new Point(443, 153);
+            comboBox1.Location = new Point(434, 156);
             comboBox1.Name = "comboBox1";
             comboBox1.Size = new Size(151, 28);
             comboBox1.TabIndex = 0;
