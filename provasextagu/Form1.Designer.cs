d35e602e689a515408c7946105de6c37bab18020
ortlieb3 <gustavooortlieb@gmail.com>
2025-06-12 05:27:09 -0300

    coloquei mais 1 button e mais 1 label no frmPrincipal

--- provasextagu/Form1.Designer.cs
+++ provasextagu/Form1.Designer.cs
@@ -34,7 +34,7 @@
             // comboBox1
             // 
             comboBox1.FormattingEnabled = true;
-            comboBox1.Location = new Point(432, 220);
+            comboBox1.Location = new Point(459, 220);
             comboBox1.Name = "comboBox1";
             comboBox1.Size = new Size(151, 28);
             comboBox1.TabIndex = 0;
