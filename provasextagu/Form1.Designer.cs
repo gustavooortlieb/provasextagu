a277f4b211877146dcfc327744109782a53a13b6
ortlieb3 <gustavooortlieb@gmail.com>
2025-06-12 17:10:42 -0300

    codei a função de apagar os dados de um dataGriedView no frmCadClientes em um button

--- provasextagu/Form1.Designer.cs
+++ provasextagu/Form1.Designer.cs
@@ -34,7 +34,7 @@
             // comboBox1
             // 
             comboBox1.FormattingEnabled = true;
-            comboBox1.Location = new Point(390, 219);
+            comboBox1.Location = new Point(399, 156);
             comboBox1.Name = "comboBox1";
             comboBox1.Size = new Size(151, 28);
             comboBox1.TabIndex = 0;
