f1f16d8492a114510ffb40b712cbf069ccc06442
ortlieb3 <gustavooortlieb@gmail.com>
2025-06-12 17:59:15 -0300

    apaguei um listBox do frmCadPedidos, n precisou

--- provasextagu/Form1.Designer.cs
+++ provasextagu/Form1.Designer.cs
@@ -34,7 +34,7 @@
             // comboBox1
             // 
             comboBox1.FormattingEnabled = true;
-            comboBox1.Location = new Point(416, 270);
+            comboBox1.Location = new Point(423, 200);
             comboBox1.Name = "comboBox1";
             comboBox1.Size = new Size(151, 28);
             comboBox1.TabIndex = 0;
